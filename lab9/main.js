c65598702a2c48c113f27b035d14a8a2b7f34e49
nelsonalmeida <160653979+nelsonFAlmeida@users.noreply.github.com>
2024-11-13 20:27:14 +0000

    lab 9 creation

---
+++ lab9/main.js
@@ -0,0 +1,59 @@
+const mainSection = document.querySelector('main section')
+const asideSection = document.querySelector('aside section')
+
+function adicionarProdutos(produto) {
+    const article = document.createElement('article')
+
+    const title = document.createElement('h3')
+    title.textContent = produto.title
+    article.append(title)
+
+    const image = document.createElement('img')
+    image.src = produto.image
+    article.append(image)
+
+    const price = document.createElement('p')
+    price.textContent = "Custo Total: " + produto.price + " €"
+    article.append(price)
+
+    const description = document.createElement('p')
+    description.textContent = produto.description
+    article.append(description)
+
+    const button = document.createElement('button')
+    button.textContent = "- Remover do carrinho"
+    /*button.addEventListener('click',) fazer uma arrow function para remover*/
+    article.append(button)
+
+}
+
+function carregarProdutos() {
+    produtos.forEach(produto => {
+        const article = document.createElement('article')
+
+        const title = document.createElement('h3')
+        title.textContent = produto.title
+        article.append(title)
+
+        const image = document.createElement('img')
+        image.src = produto.image
+        article.append(image)
+
+        const price = document.createElement('p')
+        price.textContent = "Custo Total: " + produto.price + " €"
+        article.append(price)
+
+        const description = document.createElement('p')
+        description.textContent = produto.description
+        article.append(description)
+
+        const button = document.createElement('button')
+        button.textContent = "+ Adicionar ao carrinho"
+        button.addEventListener('click',adicionarProdutos(produto))
+        article.append(button)
+
+        mainSection.append(article)
+    });
+}
+
+document.addEventListener('DOMContentLoaded',carregarProdutos())
